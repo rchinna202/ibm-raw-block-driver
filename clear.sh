5662d5b40ce1c7832913c06f3fdeda62d2ff449d
rchinna202 <62538684+rchinna202@users.noreply.github.com>
2020-09-24 15:00:26 +0530

    Create clear.sh

---
+++ clear.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /root/block-csi-driver
+
+
+oc delete project csi
+oc delete -f ibm-block-csi-operator.yaml
+rm -rf csi.ibm.com_v1_ibmblockcsi_cr.yaml
+oc delete -f array-secret.yaml
+oc delete -f block-csi-storage.yaml
+oc delete -f demo-pvc-file-system.yaml
+oc delete -f demo-pvc-raw-block.yaml
