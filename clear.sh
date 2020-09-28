398036ab889c5529489ada6426acb285283d3275
rchinna202 <62538684+rchinna202@users.noreply.github.com>
2020-09-28 20:19:48 +0530

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
 
-cd /root
 
 
 oc delete project csi
 oc delete -f ibm-block-csi-operator.yaml
+rm -rf -f ibm-block-csi-operator.yaml  
 rm -rf csi.ibm.com_v1_ibmblockcsi_cr.yaml
 oc delete -f array-secret.yaml
 oc delete -f block-csi-storage.yaml
